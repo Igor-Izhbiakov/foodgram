1821598980222378c80ab2d7c8d1e4fb96c51568
Igor Izhbiakov <firstfireinme@gmail.com>
2026-08-21 03:23:49 +0300

    again

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/backend_static'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
